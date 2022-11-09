0dab8f72ff2f9211c4f338570d27b963a7a99a8d
barbara pino <barbargustavomili@gmail.com>
2022-11-09 15:54:23 -0300

    practica de python playground intermedio

---
+++ proyectocoder/appcoder/views.py
@@ -0,0 +1,35 @@
+from django.shortcuts import render
+
+# Create your views here.
+from django.http import HttpResponse
+from appcoder.models import Curso
+from django.shortcuts import render
+def inicio(request):
+    return render(request, "appcoder/index.html")
+
+
+def cursos(request):
+    return HttpResponse("Estas en cursos")
+    return render(request, "appcoder/cursos.html")
+
+def estudiantes(request):
+    return HttpResponse("Estas en estudiantes")
+    return render(request, "appcoder/estudiantes.html")
+
+def profesores(request):
+    return HttpResponse("Estas en profesores")
+    return render(request, "appcoder/profesores.html")
+
+def entregables(request):
+    return HttpResponse("Estas en entregables")
+    return render(request, "appcoder/entregables.html")
+
+
+# def listado_cursos(request):
+#     cursos = Curso.objects.all()
+#     # Respuesta casera
+#     cadena_respuesta = "<ul>"
+#     for curso in cursos:
+#         cadena_respuesta += f"<li>({curso.nombre},{curso.camada}) </li>"
+#     cadena_respuesta += "</ul>"
+#     return HttpResponse(cadena_respuesta)
